5e11c5699362993768db46e6ffe903d4aa36caed
Roman <a1@MacBook-Pro.local>
2017-03-12 15:48:14 +0700

    redirect wrong requests to 404

--- src/app/_modules/app-routing.module.ts
+++ src/app/_modules/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoadTokenGuard } from '../_services/load-token-guard.service';
 
 const routes: Routes = [
 	{ path: '404', redirectTo: '' },
-	{ path: '**', redirectTo: '' }
+	{ path: '**', redirectTo: '404' }
 ];
 
 @NgModule({
